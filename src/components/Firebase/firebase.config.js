0249143d7eb5300072d4d13248425b495bce9d1a
Sejan Mahmud <78159075+Sejan157218@users.noreply.github.com>
2021-11-02 05:49:07 +0600

    Update firebase.config.js

--- src/components/Firebase/firebase.config.js
+++ src/components/Firebase/firebase.config.js
@@ -1,18 +1,5 @@
-// const firebaseConfig = {
-//     // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//     // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//     // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//     // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//     // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//     // appId: process.env.REACT_APP_FIREBASE_APP_ID,
 
-// };
 const firebaseConfig = {
-    apiKey: "AIzaSyBpWCpQnYhijTZq-mWcucos3iXgDnw6Kuo",
-    authDomain: "ema-john-shop-13797.firebaseapp.com",
-    projectId: "ema-john-shop-13797",
-    storageBucket: "ema-john-shop-13797.appspot.com",
-    messagingSenderId: "972562263",
-    appId: "1:972562263:web:a589d4e94519530e046812"
+
 };
 export default firebaseConfig;
